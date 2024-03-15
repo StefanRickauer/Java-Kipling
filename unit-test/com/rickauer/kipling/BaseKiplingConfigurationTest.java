ea5e41620c5f8e1da18d4d528450b2f1fced1610
Stefan Rickauer <stefanrick@gmx.de>
2024-03-15 21:15:14 +0100

    Added test case

--- unit-test/com/rickauer/kipling/BaseKiplingConfigurationTest.java
+++ unit-test/com/rickauer/kipling/BaseKiplingConfigurationTest.java
@@ -2,6 +2,9 @@ package com.rickauer.kipling;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 
 class BaseKiplingConfigurationTest {
@@ -32,5 +35,35 @@ class BaseKiplingConfigurationTest {
 		
 		Exception illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> BaseKiplingConfiguration.processCommandLineArguments(invalidArgumentArray));
 		assertEquals("Invalid argument: --invalid", illegalArgumentException.getMessage());
+	}
+	
+	@Test
+	void displayUsageMessageTest() {
+		
+		String expectedOutput = """
+				
+				Usage: Kipling <option> <argument>
+				The following options are possible: 
+					--lnch <argument> 
+						Argument has to be the path to launch4jc.exe
+					--conf <argument>
+						Argument has to be the path to the config (.xml) file
+						
+				Enter interactive mode (create configuration file):
+					--ppt
+						You will be prompted to provide the information necessary to create a config.xml file from scratch
+				""";
+
+		PrintStream standardSystemOut = System.out;
+		ByteArrayOutputStream redirectedSystemOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(redirectedSystemOut));
+        
+        BaseKiplingConfiguration.displayUsageMessage();
+        
+        String acutalOutput = redirectedSystemOut.toString();
+		assertEquals(expectedOutput.trim(), acutalOutput.trim());		// trim(): Otherwise comparison will fail due to additional line (which might be caused by redirecting output)
+		
+		// Stop redirecting output 
+		System.setOut(standardSystemOut);
 	}
 }
