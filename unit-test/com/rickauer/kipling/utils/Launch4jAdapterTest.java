8b1da3a31a2542c7beb2546124e6c3da679c8b3b
Stefan Rickauer <stefanrick@gmx.de>
2024-03-24 15:09:37 +0100

    Fixed test case

--- unit-test/com/rickauer/kipling/utils/Launch4jAdapterTest.java
+++ unit-test/com/rickauer/kipling/utils/Launch4jAdapterTest.java
@@ -11,14 +11,14 @@ public final class Launch4jAdapterTest {
 
 	static Launch4jAdapter adapter, invalidAdapter;
 	static File destinaitonFile;
-	static String configFileLocation = "C:\\Users\\noNameForM3\\Documents\\Tests\\kiplingTESTgui\\config.xml";
+	static String configFileLocation = System.getProperty("user.dir") + "\\test-data\\gui" + "\\config.xml";
 	
 	@BeforeAll
 	private static void initializeTestData() {
 		
 		adapter = new Launch4jAdapter("C:\\Program Files (x86)\\Launch4j\\launch4jc.exe");
 		invalidAdapter = new Launch4jAdapter("invalid path");
-		destinaitonFile = new File("C:\\Users\\noNameForM3\\Documents\\Tests\\kiplingTESTgui\\testGUI.exe");
+		destinaitonFile = new File(System.getProperty("user.dir") + "\\test-data\\gui" + "\\guiTest.exe");
 		
 		// Make sure files from previous test runs will be deleted before new test run
 		if (destinaitonFile.exists())
